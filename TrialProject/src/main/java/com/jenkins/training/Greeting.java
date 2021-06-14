8e03f54121435381808b312b49eb90c017b4d5f1
rajatjoshi22 <rajat4211joshi@gmail.com>
2021-06-14 15:47:17 +0530

    first commit

---
+++ TrialProject/src/main/java/com/jenkins/training/Greeting.java
@@ -0,0 +1,10 @@
+package com.jenkins.training;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("welcome from trial class");
+	}
+
+}
